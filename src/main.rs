5b8e8fc9e7d75627f9d54871e8a300215985d45c
Jordan <jordan.bankole@epitech.eu>
2022-01-23 01:07:47 +0100

    add some mpsc

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,30 @@
-use std::thread;
+#![feature(drain_filter)]
+use std::io;
+use std::sync::mpsc;
+use std::time::Duration;
 
 mod lift;
 
-
 fn main() {
-    loop {
+    let (transmitter, receiver) = mpsc::channel();
+    let mut machine = lift::mechanic::Lift::new(
+        (0..29).collect(),
+        (Duration::from_secs(4), Duration::from_secs(5)),
+        receiver,
+    );
 
+    std::thread::spawn(move || machine.run());
+    let handler = io::stdin();
+    let mut buffer = String::new();
+    loop {
+        handler.read_line(&mut buffer).unwrap();
+        let parsed = buffer.split(":").collect::<Vec<&str>>();
+        transmitter
+            .send((
+                parsed[0].parse::<isize>().unwrap(),
+                parsed[1].to_string(),
+                parsed[2].parse::<isize>().unwrap(),
+            ))
+            .unwrap();
     }
 }
