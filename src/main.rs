a610ace3752cce4ca0105a5d10191bc2a5290f07
Jordan Bankole <jordan.bankole@epitech.eu>
2022-03-11 19:15:41 +0100

    add -h

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,24 @@
 use std::io;
 use std::sync::mpsc;
 use std::time::Duration;
+use std::env;
 
 mod lift;
 
+fn helper() {
+    println!("This is a lift!");
+    println!("Just call him and he will come bring you were you wanna go");
+    println!("Call him ? Nothing easier just type: [LOCATION]:[CALLER]:[DESTINATION]");
+    println!("Example:\n\t28:thomas:5<-- will bring Thomas from the 28th floor to the 5th");
+}
+
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    
+    if args.len() == 2 && (args[1] == "-h" || args[1] == "--help") {
+        helper();
+        return;
+    }
     let (transmitter, receiver) = mpsc::channel();
     let mut machine = lift::mechanic::Lift::new(
         (0..29).collect(),
